dd950b830018f94901c173bf9124f9eee39692f6
Erick <erick100520@gmail.com>
2023-06-23 16:58:45 -0600

    clase03 completa

--- manipulando-dom-con-js-proyecto_inicial/script.js
+++ manipulando-dom-con-js-proyecto_inicial/script.js
@@ -3,6 +3,21 @@ const btn=document.querySelector('[data-form-btn]');
 const createTask=(evento)=>{
     evento.preventDefault();
     const input=document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value=input.value;
+    const list=document.querySelector('[data-list]')
+    const task=document.createElement('li');//Crea un elemento li
+    task.classList.add('card');//Agrega una clase al varible de task
+    input.value=""//Limpia los datos del input 
+    const content=`     
+    <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML=content;//innerHTML nos ayuda agragar codigo html
+
+  list.appendChild(task);//Agrega al list en este caso al ul card-list 
+  console.log(content)
+
 };
 btn.addEventListener('click',createTask);
